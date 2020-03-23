b8b7bafb12b27c937f759a757b6989adef3960ad
Wideman <wideman.christopher.roy@gmail.com>
2020-03-23 19:56:26 -0400

    I'm brain dead

--- MemeMarket/src/main/java/com/revature/data/TradeHibernate.java
+++ MemeMarket/src/main/java/com/revature/data/TradeHibernate.java
@@ -5,12 +5,14 @@ import java.util.List;
 import java.util.Set;
 
 import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.revature.beans.OwnedCard;
 import com.revature.beans.Trade;
 import com.revature.utils.HibernateUtil;
 import com.revature.utils.LogUtil;
@@ -20,6 +22,8 @@ public class TradeHibernate implements TradeDao{
 	private HibernateUtil hu = HibernateUtil.getInstance();
 	@Autowired
 	private UserDao ud;
+	@Autowired
+	private TradeStatusDao tsd;
 	
 	@Override
 	public int addTrade(Trade tr) {
@@ -54,7 +58,7 @@ public class TradeHibernate implements TradeDao{
 		boolean b = false;
 		try{
 			t = s.beginTransaction();
-			s.update(tr.getId());
+			s.update(tr);
 			t.commit();
 			b = true;
 		} catch(Exception e) {
@@ -99,4 +103,41 @@ public class TradeHibernate implements TradeDao{
 		Set<Trade> tradeSet = new HashSet<>(tradeList);
 		return tradeSet;
 	}
+	
+	public void acceptTrade(Trade t) {
+		Session s = hu.getSession();
+		Set<OwnedCard> tradedCards = t.getCardsToBeTraded();
+		t.setTradeStatus(tsd.getTradeStatus(3));
+		updateTrade(t);
+		for(OwnedCard oc : tradedCards) {
+			if(oc.getPatronId() == t.getPatronOne().getId()) {
+				oc.setPatronId(t.getPatronTwo().getId());
+			}else {
+				oc.setPatronId(t.getPatronOne().getId());
+			}
+			
+			String sql = "SELECT tradeId FROM tradeCardView WHERE ownedCardsId=:ocId AND tradeStatusId=:tsId";
+			Query<Integer> query = s.createNativeQuery(sql, Integer.class);
+			query.setParameter("employee_id", 10);
+			List<Integer> tradeIds = query.getResultList();
+			for(Integer tid : tradeIds) {
+				
+			}
+		}
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
